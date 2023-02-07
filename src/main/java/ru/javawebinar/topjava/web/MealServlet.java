80c601a1a4eb97b95c97b9a9890b5b3b7933d938
vkoko <vkokourov@gmail.com>
2023-02-07 16:11:44 +0700

    Create table, show list of meals

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,22 +1,30 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+    private static final LocalTime START_TIME = LocalTime.of(0,0);
+    private static final LocalTime END_TIME = LocalTime.of(23,59, 59);
+    private static final int CALORIES_PER_DAY = 2000;
+
+    private final MealRepository repository = new MealRepository();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("meals", MealsUtil.filteredByStreams(repository.getMeals(), START_TIME, END_TIME, CALORIES_PER_DAY));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 
     @Override
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
@@ -7,5 +9,28 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<table border="1">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+    </tr>
+    <jsp:useBean id="meals" scope="request" type="java.util.List"/>
+    <c:forEach items="${meals}" var="meal">
+        <tr>
+            <td>
+                <fmt:parseDate value="${ meal.dateTime }" pattern="yyyy-MM-dd'T'HH:mm" var="parsedDateTime"
+                               type="both"/>
+                <fmt:formatDate pattern="yyyy-MM-dd HH:mm" value="${ parsedDateTime }"/>
+            </td>
+            <td>
+                <c:out value="${meal.description}"/>
+            </td>
+            <td>
+                <c:out value="${meal.calories}"/>
+            </td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
